661530cd8f32a38bb794abcb8c373c325a0d4922
takatakayone <takahito.yoneda@atrae.co.jp>
2020-09-20 11:56:48 +0900

    chore

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+class App extends Component {
+  render() {
+    return <h1>"HEYHEY"</h1>
+  }
+}
+
+export default App;
